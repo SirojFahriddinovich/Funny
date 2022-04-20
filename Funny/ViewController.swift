e3f071144d045d0c15d60354ace43a126eb1bbbf
SirojFahriddinovich <sirojiddinqutbiddinov2@gmail.com>
2022-04-20 12:16:26 +0500

    Initial Commit

---
+++ Funny/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Funny
+//
+//  Created by Macbook Air on 20/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
